299b3bd0e32b93d68db471d449bdf7a7b6c89447
kenkuts <kennethchoi19@gmail.com>
2020-04-15 00:57:02 +0300

    commit

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :ana
+
+    def initialize(ana)
+        @ana = ana
+    end
+
+    def match(array)
+        ans = []
+        array.each do |ele|
+            ans << ele if @ana.split("").sort == ele.split("").sort
+        end
+        ans
+    end
+end
